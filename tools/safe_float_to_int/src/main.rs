edc418451efc4f2e83067996f9aeae7062ee58f8
Eric Seppanen <eds@reric.net>
2023-12-01 15:45:57 -0800

    tools: use hexfloat2 crate for float formatting

--- tools/safe_float_to_int/src/main.rs
+++ tools/safe_float_to_int/src/main.rs
@@ -2,8 +2,8 @@
 //!
 //! This is to verify *exactly* what the safe range for float-to-int conversions is.
 
+use hexfloat2::HexFloat;
 use ieee754::Ieee754;
-use std::fmt;
 
 macro_rules! limits {
     ($src:ty => $dst:ident; < $min:expr, > $max:expr) => {
@@ -21,8 +21,8 @@ macro_rules! limits {
                 let next_int = next as $dst;
                 let next_rnd = next_int as $src;
                 if next_rnd != next {
-                    println!("safe {} max: {}, {:e}, {:+x}",
-                        stringify!($src => $dst), cur, cur, FloatHex(cur));
+                    println!("safe {} max: {}, {:e}, {:}",
+                        stringify!($src => $dst), cur, cur, HexFloat(cur));
                     break;
                 } else {
                     cur = next;
@@ -41,8 +41,8 @@ macro_rules! limits {
                 let next_int = next as $dst;
                 let next_rnd = next_int as $src;
                 if next_rnd != next {
-                    println!("safe {} min: {:+}, {:+e}, {:+x}",
-                        stringify!($src => $dst), cur, cur, FloatHex(cur));
+                    println!("safe {} min: {:+}, {:+e}, {}",
+                        stringify!($src => $dst), cur, cur, HexFloat(cur));
                     break;
                 } else {
                     cur = next;
@@ -70,121 +70,4 @@ fn main() {
     limits!(f64 => u16; > 64000.0);
     limits!(f64 => u32; > 4290000000.0);
     limits!(f64 => u64; > 18446744073709500000.0);
-}
-
-fn format_hex_parts(
-    f: &mut fmt::Formatter,
-    leading: char,
-    sig: impl Into<u64>,
-    exp: impl Into<i64>,
-) -> fmt::Result {
-    let sig = sig.into();
-    let exp = exp.into();
-    let sig_fmt = format!("{:x}", sig);
-    let mut sig_fmt = sig_fmt.trim_end_matches('0');
-    if sig_fmt.is_empty() {
-        sig_fmt = "0";
-    }
-    write!(f, "0x{leading}.{sig_fmt}p{exp}")
-}
-struct FloatHex<T>(pub T);
-
-impl fmt::LowerHex for FloatHex<f32> {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        use std::num::FpCategory;
-
-        let v = self.0;
-
-        match v.classify() {
-            FpCategory::Nan => {
-                fmt.write_str("nan")?;
-            }
-            FpCategory::Infinite => {
-                if v.is_sign_negative() {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                fmt.write_str("infinity")?;
-            }
-            FpCategory::Zero => {
-                if v.is_sign_negative() {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                fmt.write_str("0x0p0")?;
-            }
-            FpCategory::Subnormal => {
-                let (neg, exp, sig) = v.decompose();
-                if neg {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                format_hex_parts(fmt, '0', sig << 1, exp)?;
-            }
-            FpCategory::Normal => {
-                let (neg, exp, sig) = v.decompose();
-                if neg {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                format_hex_parts(fmt, '1', sig << 1, exp)?;
-            }
-        }
-
-        Ok(())
-    }
-}
-
-impl fmt::LowerHex for FloatHex<f64> {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        use std::num::FpCategory;
-
-        let v = self.0;
-
-        match v.classify() {
-            FpCategory::Nan => {
-                fmt.write_str("nan")?;
-            }
-            FpCategory::Infinite => {
-                if v.is_sign_negative() {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                fmt.write_str("infinity")?;
-            }
-            FpCategory::Zero => {
-                if v.is_sign_negative() {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                fmt.write_str("0x0p0")?;
-            }
-            FpCategory::Subnormal => {
-                let (neg, exp, sig) = v.decompose();
-                if neg {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                format_hex_parts(fmt, '0', sig << 1, exp)?;
-            }
-            FpCategory::Normal => {
-                let (neg, exp, sig) = v.decompose();
-                if neg {
-                    fmt.write_str("-")?;
-                } else if fmt.sign_plus() {
-                    fmt.write_str("+")?;
-                }
-                format_hex_parts(fmt, '1', sig << 1, exp)?;
-            }
-        }
-
-        Ok(())
-    }
 }
